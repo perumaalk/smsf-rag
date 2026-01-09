a63bab38126635195842c746ca964a2ea53227f0
perumaalk <perumaalk@users.noreply.github.com>
2026-01-09 06:47:06 +0000

    API Key Header Correction

--- app/api/dependencies.py
+++ app/api/dependencies.py
@@ -6,9 +6,12 @@ from fastapi.security.api_key import APIKeyHeader
 import os
 
 API_KEY_NAME = "X-Internal-Api-Key"
-api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+api_key_header = APIKeyHeader(name="INTERNAL_API_KEY", auto_error=False)
+
 
 async def verify_api_key(api_key: str = Security(api_key_header)):
+    print(api_key)
+    print(os.getenv("INTERNAL_API_KEY"))
     if api_key == os.getenv("INTERNAL_API_KEY"):
         return api_key
     raise HTTPException(
